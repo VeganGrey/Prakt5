27adfba8dddc60f3093149487d0be2543972f138
doctorflex <doctorflex@DESKTOP-RQVSRNU>
2022-10-17 18:13:15 +0300

    Апдейт 2.0

--- Class1.cs
+++ Class1.cs
@@ -31,18 +31,29 @@ namespace Prakt5
         /// <summary>
         /// Метод для нахождения произведния чисел в первой паре
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Произведение первого числа пары на второе</returns>
         public int Peremnozh()
         {
             return Pair1 * Pair2;
         }
-
+        /// <summary>
+        /// Метод для нахождения разности первой пары и второй пары
+        /// </summary>
+        /// <param name="value">Объект класса обозначающий вторую пару</param>
+        /// <param name="dop1">Значение от разности первого числа перв пары и второй пары</param>
+        /// <param name="dop2">Значение от разности второго числа перв пары и второй пары</param>
         public void Vichit(Pair value,out int dop1,out int dop2)
         {
             dop1 = Pair1 - value.Pair1;
             dop2 = Pair2 - value.Pair2;
         }
-        
+        /// <summary>
+        /// Метод для нахождения разности первой,второй и третьей пары
+        /// </summary>
+        /// <param name="value1">Объект обозначающий вторую пару</param>
+        /// <param name="value2">Объект обозначающий третью пару</param>
+        /// <param name="dop1">Значение разности первых чисел перв пары,второй и третьей</param>
+        /// <param name="dop2">Значение разности вторых чисел перв пары,второй и третьей</param>
         public void Vichit(Pair value1,Pair value2,out int dop1, out int dop2)
         {
             dop1 = Pair1 - value1.Pair1 - value2.Pair1;
